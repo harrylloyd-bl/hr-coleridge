8b94213f8defc406ac199be1c94668db65650aa6
harrylloyd-bl <harry.lloyd@bl.uk>
2025-09-24 15:13:26 +0100

    Correct misformatted Unicode in csv output

--- extract_entities.py
+++ extract_entities.py
@@ -78,8 +78,8 @@ if __name__ == "__main__":
                     if line[2][0].text is None:
                         continue
                     line_attributes = parse_attributes(line.attrib.get("custom", []))
-                    print(line_attributes)
-                    print(line[2][0].text)
+                    # print(line_attributes)
+                    # print(line[2][0].text)
                     if "survey_party" in line_attributes:
                         survey_party_lines.append(extract_attribute_text(line, "survey_party", line_attributes)) 
                     if "survey_area" in line_attributes:
@@ -105,8 +105,8 @@ if __name__ == "__main__":
                 for j, line in enumerate(region[1:-1]):  # TODO add 'continued logic'
                     entity = {}
                     line_attributes = parse_attributes(line.attrib.get("custom", []))
-                    print(line_attributes)
-                    print(line[2][0].text)
+                    # print(line_attributes)
+                    # print(line[2][0].text)
                     if "survey_party" in line_attributes:
                         survey_party = extract_attribute_text(line, "survey_party", line_attributes)
 
@@ -179,9 +179,9 @@ if __name__ == "__main__":
             continue
 
         entity_df = pd.DataFrame(entities)
-        entity_df.info()
+        # entity_df.info()
 
-        print(entity_df.head())
+        # print(entity_df.head())
         entity_dfs.append(entity_df)
     
     combined_entities = pd.concat(entity_dfs)
@@ -191,8 +191,20 @@ if __name__ == "__main__":
             "survey_party", "survey_area", "place", "military_branch", "ethnicity", "ethnicity_text", "dateOfDeath", "medical", "wikiData", "placeName",  "continued"
         ]
     ]
+    
+    missing_cols = combined_entities.columns.difference(combined_entities_ordered.columns)
 
-    print(f"{combined_entities.columns.difference(combined_entities_ordered.columns)} not ordered in output")
+    if not missing_cols.empty:
+        print(f"{missing_cols} not ordered in output")
+
+    # Handle misformatted Unicode, U+0020 (space), U+0027 (apostrophe)
+    combined_entities_ordered["title"] = combined_entities_ordered["title"].str.replace(r"\u0020", " ")
+    combined_entities_ordered["firstname"] = combined_entities_ordered["firstname"].str.replace(r"\u0020", " ")
+    combined_entities_ordered["firstname"] = combined_entities_ordered["firstname"].str.replace(r"\u0027", "'")
+    combined_entities_ordered["lastname"] = combined_entities_ordered["lastname"].str.replace(r"\u0020", " ")
+    combined_entities_ordered["lastname"] = combined_entities_ordered["lastname"].str.replace(r"\u0027", "'")
+    combined_entities_ordered["heading_placeName"] = combined_entities_ordered["heading_placeName"].str.replace(r"\u0020", " ")
+    combined_entities_ordered["placeName"] = combined_entities_ordered["placeName"].str.replace(r"\u0020", " ")
 
     combined_entities_ordered.to_csv("data/processed/combined_entities.csv", encoding="utf8")
     combined_entities_ordered.groupby(by="report_date").count().apply(lambda x: logging.info(f"{int(x.name)} {x['person']} entities"), axis=1)
