dd87e1c791e2cdc84f11e02fdbdd348b467ac7fa
harrylloyd-bl <harry.lloyd@bl.uk>
2025-09-24 12:22:14 +0100

    Add '\' to attribute regex to catch misformated \u0020 spaces

--- coleridge/data/parse_xml.py
+++ coleridge/data/parse_xml.py
@@ -1,3 +1,4 @@
+from collections import Counter
 import re
 from xml.etree.ElementTree import Element
 
@@ -11,11 +12,13 @@ def parse_attributes(attrib: str):
     Returns:
         dict[str, str]: A well formatted dictionary of attributes
     """
-    attrib_pair_re = re.compile(r"(?P<tag>\w+) (?P<text>\{[\.\w\s:;\d]+\})")
-    attrib_inner_re = re.compile(r"(?P<tag>\w+):(?P<text>[\.\w\s\d]+)")
+    attrib_pair_re = re.compile(r"(?P<tag>\w+) (?P<text>\{[\.\w\s:;\d\\]+\})")
+    attrib_inner_re = re.compile(r"(?P<tag>\w+):(?P<text>[\.\w\s\d\\]+)")
     all_attribs = attrib_pair_re.findall(attrib)
+    # counts = Counter([x[0] for x in all_attribs])  # TODO implement checking for multiple attrs in one line
     inner_found = {k:attrib_inner_re.findall(v[1:-1]) for k,v in all_attribs}
-    return {k0: {k1:v1 for k1, v1 in v0} for k0, v0 in inner_found.items()}
+    formatted_attributes = {k0: {k1:v1 for k1, v1 in v0} for k0, v0 in inner_found.items()}
+    return formatted_attributes
 
 
 def extract_attribute_text(line: Element, attrib_name: str, attribs: dict[any, any]):
@@ -37,3 +40,19 @@ def extract_attribute_text(line: Element, attrib_name: str, attribs: dict[any, a
     
     offset, length = int(attribs[attrib_name].pop("offset")), int(attribs[attrib_name].pop("length"))
     return line[2][0].text[offset:offset + length]
+
+
+def strip_debug_log(filepath):
+    with open(filepath) as f:
+        lines = f.readlines()
+        stripped_lines = [l for l in lines if "DEBUG" in l]
+    
+    with open(filepath[:-4] + "_stripped.log", "w") as f:
+        f.write(stripped_lines)
+
+
+if __name__ == "__main__":
+    attr_str = "readingOrder {index:28;} acknowledgement {offset:0; length:11; continued:true;} person {offset:20; length:6;title:Mr; lastname:Baness;} person {offset:28; length:5;title:Mr; lastname:Horst;} person {offset:38; length:5;title:Mr; lastname:Bolst;} acknowledgement {offset:48; length:11; continued:true;}"
+    parse_attributes(attr_str)
+
+
